c787a8b8c7022a2ae77db66e792edfedc4bf845a
GuilhermeLima33 <guigui30042008@gmail.com>
2026-05-28 11:42:36 -0300

    .

---
+++ 2026-main/Juliana/Projetos/Pedidos/validadordesenhaatt/script.js
@@ -0,0 +1,31 @@
+let senha = document.getElementById("senha")
+let confirmar = document.getElementById("confirmar")
+let mensagem = document.getElementById("mensagem")
+
+confirmar.addEventListener("input", function() {
+
+let numeros = /[0-9]/.test(senha.value)
+let letras = /[a-zA-Z]/.test(senha.value)
+let especial = /[!@#$%&*]/.test(senha.value)
+let tamanho = senha.value.length >= 12
+
+    // if(senha.value == confirmar.value){
+    //     mensagem.innerText = "As senhas são iguais"
+    //     mensagem.classList.add("certo")
+    //     mensagem.classList.remove("errado")
+    // }else{
+    //     mensagem.innerText = "As senhas são iguais"
+    //     mensagem.classList.remove("certo")
+    //     mensagem.classList.add("errado") 
+    // }
+
+    if(senha.value == confirmar.value && numeros && letras && especial && tamanho){
+        mensagem.innerText = "As senhas são iguais"
+        mensagem.classList.add("certo")
+        mensagem.classList.remove("errado")
+    }else{
+        mensagem.innerText = "As senhas precisa ter letras, números, caracter especial, ter no mínimo 10 caracteres e ser igual"
+        mensagem.classList.remove("certo")
+        mensagem.classList.add("errado") 
+    }
+})
